2cda87386c8963b8a3bf98cf693275a0ddbdc056
Chaitanya7725 <chaitanya7725@gmail.com>
2024-03-12 18:38:46 -0500

    Initial Commit

---
+++ emoji/emojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  emojiApp.swift
+//  emoji
+//
+//  Created by Chaitanya Takalikar on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct emojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
